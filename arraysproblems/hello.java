0410b4bcfe93c86ea4f300ebb758aa91e17b8447
Teddu Lokesh <119160646+TedduLokesh@users.noreply.github.com>
2025-07-06 09:04:29 +0530

    Update hello.java

--- arraysproblems/hello.java
+++ arraysproblems/hello.java
@@ -2,6 +2,17 @@ package arraysproblems;
 
 public class hello {
     public static void main(String[] args) {
-
+        // if the given number is satisfied with in the range then say hello! other wise bye!
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int GuessNumber = sc.nextInt();
+        for (int i=n1; i <= n2; i++){
+             if (GuessNumber == n1){
+                 System.out.println("Hello");
+                 break;
+             }
+        }
+        System.out.println("Bye!");
     }
 }
